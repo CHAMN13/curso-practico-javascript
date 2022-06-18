ce045f0d48d5d29c17ed96dc072352865184b60b
wfriagas4s <walter.friagas@grupo4s.com>
2022-06-18 18:40:58 -0500

    calculadora cuadrado

--- figuras.js
+++ figuras.js
@@ -50,3 +50,21 @@ function areaCirculo (radio){
 }
 
 console.groupEnd();
+
+
+// Conectar con formulario
+function calcularPerimetroCuadrado(){
+  const input = document.getElementById("inputCuadrado");
+  const value = input.value;
+
+  const perimetro = perimetroCuadrado(value);
+  alert(perimetro);
+}
+
+function calcularAreaCuadrado(){
+  const input = document.getElementById("inputCuadrado");
+  const value = input.value;
+
+  const area = areaCuadrado(value);
+  alert(area);
+}
